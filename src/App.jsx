1b4ed36dca7b930d4716732383ff4c636871e7bf
Samuel Berrus <samuelberrus@gmail.com>
2021-08-31 02:43:14 +0200

    Creando nuevo repo + estructura proyecto + git remote

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Routes from "./components/routes/Routes";
+import Header from "./components/_partials/Header";
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Routes />
+        </div>
+    );
+}
+
+export default App;
